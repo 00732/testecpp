62464385dabfc1a84c38ac56ded31a05e85f2b7c
00732 <daviassuncao027@gmail.com>
2024-03-14 10:13:24 -0300

    improve

--- RungeGutta.hh
+++ RungeGutta.hh
@@ -1,10 +1,20 @@
-#ifndef RungeGutta_hh
-#define RungeGutta_hh
+// RungeGutta.hh
 
-void   interpolate_double(double angle[], double ai, double an, double n);
+#ifndef RUNGE_GUTTA_HH
+#define RUNGE_GUTTA_HH
 
-double f(double x, double y, double z); // must apear in main.cc
-double g(double x, double y, double z); // must apear in main.cc
+// Interpolation function for double precision values.
+void interpolate_double(double angle[], double ai, double an, double n);
+
+// Function prototypes for the ODE right-hand side functions.
+double f(double x, double y, double z);
+double g(double x, double y, double z);
+
+// Runge-Kutta 4th order integration function.
 double runge_kutta_4(double x, double y, double *z, double h);
 
-#endif // RungeGutta_hh
+// Internal helper functions.
+static inline double pow2(double x) { return x * x; }
+static inline double pow3(double x) { return pow2(x) * x; }
+
+#endif // RUNGE_GUTTA_HH
